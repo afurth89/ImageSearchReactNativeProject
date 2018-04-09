e3336e746832de3c88a62988d95d8200deecad00
Andrew Furth <andrew@versame.com>
2018-04-08 20:06:49 -0400

    fetch and render collection of hardcoded images from Pixabay

--- App.js
+++ App.js
@@ -29,22 +29,34 @@ class SearchInput extends Component {
 class SearchResults extends Component {
   constructor(props) {
     super(props);
+    this.state = { isLoading: true }
   }
+  
+  componentDidMount() {
+    return fetch('https://pixabay.com/api/?key=8622479-43a84036a79d6b86f0d2526cd&q=yellow+flowers&image_type=photo&pretty=true')
+      .then((response) => response.json())
+      .then((responseJson) => {
+        console.log(responseJson.hits);
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson.hits
+        }, function() {
+          
+        });
+
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
   render() {
     return (
       <View style={styles.searchResults}>
         <FlatList 
-          data={[
-            {key: 'Devin'},
-            {key: 'Jackson'},
-            {key: 'James'},
-            {key: 'Joel'},
-            {key: 'John'},
-            {key: 'Jillian'},
-            {key: 'Jimmy'},
-            {key: 'Julie'},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          data={this.state.dataSource}
+          renderItem={ ({item}) => <Image source={{uri:item.largeImageURL}} style={styles.item}/> }
+          keyExtractor={(item, index) => index}
         />
       </View>
     )
@@ -80,7 +92,7 @@ const styles = StyleSheet.create({
   },
   item: {
     padding: 10,
-    fontSize: 36,
-    height: 44,
+    width: 193, 
+    height: 110
   }
 });
